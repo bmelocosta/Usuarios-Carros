86c8ca0e8951b79a7393be6b41d182e810f69438
bmelocosta <bmelocosta@gmail.com>
2023-12-02 11:30:23 -0300

    Historia_2

---
+++ Usuarios-Carros/src/main/java/com/usuarioscarros/crm/UsuariosCarrosApplication.java
@@ -0,0 +1,13 @@
+package com.usuarioscarros.crm;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UsuariosCarrosApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UsuariosCarrosApplication.class, args);
+	}
+
+}
---
+++ Usuarios-Carros/src/test/java/com/usuarioscarros/crm/UsuariosCarrosApplicationTests.java
@@ -0,0 +1,13 @@
+package com.usuarioscarros.crm;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UsuariosCarrosApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
