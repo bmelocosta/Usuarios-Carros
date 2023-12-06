4cca94be3481cc92eef99e6f5ec83557b3d00ad0
bmelocosta <bmelocosta@gmail.com>
2023-12-06 11:31:54 -0300

    Historia_9

--- uc-api/src/main/java/com/usuarioscarros/crm/repository/UsuarioRepository.java
+++
@@ -1,9 +0,0 @@
-package com.usuarioscarros.crm.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.usuarioscarros.crm.model.Usuario;
-
-public interface UsuarioRepository extends JpaRepository<Usuario, Long>{
-
-}
